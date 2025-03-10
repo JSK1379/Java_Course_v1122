30b256433ebf78e6c2aa1007cd279e7fba66c488
JSK1379 <js13579K@gmail.com>
2025-03-11 02:03:15 +0800

    racer

---
+++ FCPS_eUnits/Unit1/src/BoxTopRacer.java
@@ -0,0 +1,15 @@
+import edu.fcps.karel2.Robot;
+import edu.fcps.karel2.Display;
+
+   public class BoxTopRacer extends Racer
+       {
+        public BoxTopRacer(int y) 
+         {
+            super(y);
+         }
+       @Override
+       public void jumpRight()
+        {
+           
+        }
+   	}
---
+++ FCPS_eUnits/Unit1/src/SteepleChaseRacer.java
@@ -0,0 +1,15 @@
+import edu.fcps.karel2.Robot;
+import edu.fcps.karel2.Display;
+
+   public class SteepleChaseRacer extends Racer
+       {
+        public SteepleChaseRacer(int y) 
+         {
+            super(y);
+         }
+       @Override
+       public void jumpRight()
+        {
+           
+        }
+   	}
