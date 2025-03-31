d59b93143b0cc45fbd7c3ba2940b7ae2d4659d9f
JSK1379 <js13579K@gmail.com>
2025-03-31 23:43:44 +0800

    02.2

--- FCPS_eUnits/Unit2/src/Lab02/Driver02.java
+++ FCPS_eUnits/Unit2/src/Lab02/Driver02.java
@@ -5,8 +5,8 @@ public class Driver02
 
    public static void main(String[] args){
    
-      JFrame frame = new JFrame("Lab01");
-      frame.setSize(400,250);
+      JFrame frame = new JFrame("Lab02");
+      frame.setSize(400,400);
       frame.setLocation(100,50);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setContentPane(new Panel02());
--- FCPS_eUnits/Unit2/src/Lab02/Panel02.java
+++ FCPS_eUnits/Unit2/src/Lab02/Panel02.java
@@ -4,9 +4,12 @@ import java.awt.*;
 public class Panel02 extends JPanel{
    
    public void paintComponent(Graphics g){
-      
+      g.setColor(Color.RED);
+      g.fillRect(0,0,400,400);
       ImageIcon thomas = new ImageIcon("tj.jpg");
       g.drawImage(thomas.getImage(),50,50,null);
 //      g.drawImage(thomas.getImage(),50,50,25,75,null);
+      
+      
    }
 }
