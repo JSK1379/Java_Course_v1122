0157884565d2482cc5b789806ce0c43c2f93632f
JSK1379 <js13579K@gmail.com>
2025-04-07 15:24:20 +0800

    z

---
+++ FCPS_eUnits/Unit1/src/Lab.java
@@ -0,0 +1,20 @@
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+
+public class Lab {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Display.openWorld("../maps/maze1r.map");
+		Display.setSize(10,10);
+      Display.setSpeed(5);
+
+
+	}
+
+}
--- FCPS_eUnits/Unit1/src/Lab10.java
+++ FCPS_eUnits/Unit1/src/Lab10.java
@@ -10,7 +10,7 @@ public class Lab10 {
 		// TODO Auto-generated method stub
 
       
-      Display.openWorld("../maps/maze2.map");
+      Display.openWorld("../maps/maze9r.map");
 		Display.setSize(10,10);
       Display.setSpeed(10);
 
