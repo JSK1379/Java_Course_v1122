177f9c7fa8d42e0b498c15c8a3944d16424fc007
JSK1379 <js13579K@gmail.com>
2025-04-14 21:46:23 +0800

    8

--- FCPS_eUnits/Unit2/src/Lab08/Driver08.java
+++ FCPS_eUnits/Unit2/src/Lab08/Driver08.java
@@ -8,10 +8,18 @@
       {
       
       	/************************/
+         JFrame frame = new JFrame("Flower Turtles");
+         frame.setSize(700, 700);
+         frame.setLocation(200, 100);
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setContentPane(new TurtlePanel());
+         frame.setVisible(true);
+         Turtle.clear(Color.white);
+         
       	FlowerTurtle a = new FlowerTurtle(100,42,Color.red);
-         FlowerTurtle b = new FlowerTurtle(100,42,Color.yellow);
-         FlowerTurtle c = new FlowerTurtle(100,42,Color.blue);
-         FlowerTurtle d = new FlowerTurtle(100,42,Color.black);
+         FlowerTurtle b = new FlowerTurtle(250,42,Color.yellow);
+         FlowerTurtle c = new FlowerTurtle(400,42,Color.blue);
+         FlowerTurtle d = new FlowerTurtle(550,42,Color.black);
          a.drawShape();
          b.drawShape();
          c.drawShape();
