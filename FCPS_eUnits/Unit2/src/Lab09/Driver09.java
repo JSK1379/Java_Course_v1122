02aecbf845c496228f6586b73ca08b0840ad99a3
JSK1379 <js13579K@gmail.com>
2025-04-14 22:42:32 +0800

    09

--- FCPS_eUnits/Unit2/src/Lab09/Driver09.java
+++ FCPS_eUnits/Unit2/src/Lab09/Driver09.java
@@ -7,8 +7,7 @@
       public static void main(String[] args)
       {
       
-      	/************************/
-         JFrame frame = new JFrame("Polygun Turtles");
+         JFrame frame = new JFrame("Turtles");
          frame.setSize(400, 400);
          frame.setLocation(300, 100);
          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,12 +16,13 @@
          Turtle.clear(Color.white);
          
       	TwistyTurtle a = new TwistyTurtle(1);
-         TwistyTurtle b = new TwistyTurtle(250,42,Color.yellow);
-         TwistyTurtle c = new TwistyTurtle(400,42,Color.blue);
-         a.drawShape();
-         b.drawShape();
-         c.drawShape();
-      	/************************/
-      
+         TwistyTurtle b = new TwistyTurtle(100,250,90,2);
+         TwistyTurtle c = new TwistyTurtle(500,350,90,3);
+         Thread t1 = new Thread(a);
+         Thread t2 = new Thread(b);
+         Thread t3 = new Thread(c);
+         t1.start();
+         t2.start();
+         t3.start();
       }
    }
--- FCPS_eUnits/Unit2/src/Lab09/TwistyTurtle.java
+++ FCPS_eUnits/Unit2/src/Lab09/TwistyTurtle.java
@@ -1,10 +1,11 @@
 	//Name______________________________ Date_____________
    import edu.fcps.Turtle;
    import java.awt.Color;
-   public class TwistyTurtle extends Turtle
+   public class TwistyTurtle extends Turtle implements Runnable
    {
       private double mySize;
       private Color myColor;
+      private int drawmode;
       public TwistyTurtle(int m)
       {
          super();
@@ -19,16 +20,16 @@
          drawmode = m;
       }
       
-      public TwistyTurtle(double x, double y, double heading, int m)
+      public TwistyTurtle(double x, double y, double h, int m)
       {
-         super(x,y,heading);
+         super(x,y,h);
          mySize = 30.0;
          drawmode = m;
       }
       
-      public TwistyTurtle(double x, double y, double heading, double size,int m)
+      public TwistyTurtle(double x, double y, double h, double size,int m)
       {
-         super(x,y,heading);
+         super(x,y,h);
          mySize = size;
          drawmode = m;
       }
@@ -38,9 +39,45 @@
          mySize = size;
       }
       
+      public void rotatetriangle(){
+         for(int i=5;i<=400;i+=10){
+            forward(i);
+            turnLeft(123);
+         }
+      }
+      
+      public void rotatet(){
+         for(int i=0;i<=150;i+=2){
+            forward(i);
+            turnLeft(30);
+         }
+      }
+      
+      public void rotatesquare(){
+         for(int i=25;i<=150;i+=5){
+            forward(i);
+            turnLeft(45);
+            forward(i);
+            turnLeft(90);
+            forward(i);
+            turnLeft(90);
+            forward(i);
+            turnLeft(90);
+         }
+      }
+      
+      
+      
+      public void octagon(){
+         for(int i=25;i<=150;i+=5){
+            forward(i);
+            turnLeft(123);
+         }
+      }
+      
       public void drawShape()
       {
-         switch(m){
+         switch(drawmode){
             case 1:
                super.setColor(Color.orange);
                rotatetriangle();
@@ -53,11 +90,12 @@
                super.setColor(Color.red);
                rotatesquare();
                break;
-            case 4:
-               super.setColor(Color.yellow);
-               octagon();
-               break;
-         
          }
+      }
+      
+      
+      public void run(){
+         drawShape();
+      
       }
    }
