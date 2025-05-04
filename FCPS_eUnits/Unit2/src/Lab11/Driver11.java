a36c658e2266dcd2a1ee9db785bcc4e391c86e4b
JSK1379 <js13579K@gmail.com>
2025-05-04 22:58:06 +0800

    11.2

--- FCPS_eUnits/Unit2/src/Lab11/Driver11.java
+++ FCPS_eUnits/Unit2/src/Lab11/Driver11.java
@@ -8,7 +8,7 @@
       { 
          JFrame frame = new JFrame("Unit2, Lab11");
          frame.setSize(400, 400);
-         frame.setLocation(0, 0);
+         frame.setLocation(100, 200);
          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        	frame.setContentPane(new PinballPanel());
          frame.setVisible(true);
