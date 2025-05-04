ae8771ec653d014309d4ab9dcd496a7b55c648e6
JSK1379 <js13579K@gmail.com>
2025-05-04 23:01:29 +0800

    11

--- FCPS_eUnits/Unit2/src/Lab11/Ball.java
+++ FCPS_eUnits/Unit2/src/Lab11/Ball.java
@@ -42,16 +42,25 @@
        public void move(double rightEdge, double bottomEdge)
       {
          setX(getX()+ dx);                    // x = x + dx
-        
+         setY(getY()+ dy);  
         // check for left & right edge bounces
          if(getX() >= rightEdge - getRadius())     //hits the right edge
          {
             setX(rightEdge - getRadius());
             dx = dx * -1; 
          }
-         /*else if {
-            
-         }*/
-         
+         else if(getX()<=getRadius()){
+            setX(getRadius());
+            dx = dx * -1; 
+         }
+         if(getY() >= bottomEdge - getRadius())
+         {
+            setY(bottomEdge - getRadius());
+            dy = dy * -1; 
+         }
+         else if(getY()<=getRadius()){
+            setY(getRadius());
+            dy = dy * -1; 
+         }
       }
    }
